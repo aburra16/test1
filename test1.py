ef08a92ec1918ca57fee3e4052daf09c90df596f
Home <asb2005@columbia.edu>
2017-01-26 13:30:22 -0500

    Update 1/26 1:30pm; Reading CSV into objects, manipulating and outputing into CSV

--- test1.py
+++ test1.py
@@ -10,14 +10,73 @@ Date = []
 Amount = []
 Payer = []
 
-next(csvfl)
-for row in csvfl:
-    f1 = row[0]
-    f2 = row[1]
-    f3 = row[2]
-    Date.append(f1)
-    Amount.append(float(f3))
-    Payer.append(f2)
+ClaimList = []
 
-print(*Amount, sep="\n")
-print("Mean :", np.mean(Amount))
+PayerList = []
+
+next(csvfl)
+# for row in csvfl:
+#     f1 = row[0]
+#     f2 = row[1]
+#     f3 = row[2]
+#     Date.append(f1)
+#     Amount.append(float(f3))
+#     Payer.append(f2)
+#
+# print(*Amount, sep="\n")
+# print("Mean :", np.mean(Amount))
+
+class Family:
+
+    def __init__(self, name, count, amount):
+        self.name = name
+        self.count = count
+        self.amount = amount
+
+class Claim:
+
+    clmCount = 0
+
+    def __init__(self, clmDate, clmPayer, clmAmount):
+        self.clmDate = clmDate
+        self.clmPayer = clmPayer
+        self.clmAmount = clmAmount
+        Claim.clmCount +=1
+
+#Populate claim list from CSV
+for row in csvfl:
+    PayerList.append(row[1])
+    ClaimList.append(Claim(row[0],row[1], float(row[2])))
+
+UniquePayers = list(set(PayerList))
+
+Aetna = []
+Medicare = []
+Empire = []
+FamilyList = []
+
+#Create blank FamilyList with Payer Name
+for i in range(len(UniquePayers)):
+    iFam = Family(UniquePayers[i],0,0)
+    FamilyList.append(iFam)
+
+for cl in ClaimList:
+    for i in range(len(FamilyList)):
+        if cl.clmPayer == FamilyList[i].name:
+            FamilyList[i].amount += cl.clmAmount
+            FamilyList[i].count += 1
+
+
+outputFile = open("OutputCSV.csv", "w")
+csvWR = csv.writer(outputFile)
+CSVheader = ["Insurance", "Count", "Amount", "Average"]
+csvWR.writerow(CSVheader)
+
+DataOut = []
+
+for i in range(len(FamilyList)):
+    print(FamilyList[i].name, ": ", "Count = ", FamilyList[i].count, "; Amount = ", FamilyList[i].amount, sep='')
+    average = float(FamilyList[i].amount) / float(FamilyList[i].count)
+    csvWR.writerow([FamilyList[i].name, FamilyList[i].count, FamilyList[i].amount, average])
+
+fl.close()
