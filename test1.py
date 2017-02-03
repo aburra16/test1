78272f4d6c0093f00e73ddcd09d49f389bb0dafb
Home <asb2005@columbia.edu>
2017-02-03 18:52:05 -0500

    Updates claims with remit amounts and times

--- test1.py
+++ test1.py
@@ -11,25 +11,36 @@ ClaimList = []
 ProcedureList = []
 PayerList = []
 AllowableList = []
+FamilyList = []
 
+#Arrays for randomization of payment time and haircut
+avgTime = [27,29,14,23,12,16,25]
+stdTime = [0.1,0.14,0.12,0.04,0.13,0.07,0.16]
+avgHaircut = [0.12,0.35,0.42,0.27,0.19,0.44,0.28]
+stdHaircut = [0.11,0.16,0.05,0.19,0.22,0.38,0.08]
 
 class Family:
 
-    def __init__(self, name, count, amount):
+    def __init__(self, name, count, avgTime, stdTime, haircut, stdHaircut):
         self.name = name
         self.count = count
-        self.amount = amount
+        self.avgTime = avgTime
+        self.stdTime = stdTime
+        self.haircut = haircut
+        self.stdHaircut = stdHaircut
 
 class Claim:
 
     clmCount = 0
 
-    def __init__(self, clmDate, clmHosp, clmPayer, clmProcedure, clmAmount):
+    def __init__(self, clmDate, clmHosp, clmPayer, clmProcedure, clmAmount,remitAmount, remitTime):
         self.clmDate = clmDate
         self.clmHosp = clmHosp
         self.clmPayer = clmPayer
         self.clmProcedure = clmProcedure
         self.clmAmount = clmAmount
+        self.remitAmount = remitAmount
+        self.remitTime = remitTime
         Claim.clmCount +=1
 
 class PayerAllowable:
@@ -42,9 +53,15 @@ class PayerAllowable:
 
 #Populate allowable amounts list from CSV
 PayerList = next(csvfl1)
+PayerList.pop(0)
+
+    #first set up families
+for i in range(len(PayerList)):
+    FamilyList.append(Family(PayerList[i],0,avgTime[i], stdTime[i], avgHaircut[i], stdHaircut[i]))
+
 for row in csvfl1:
-    for i in range(1,len(PayerList)):
-        AllowableList.append(PayerAllowable(PayerList[i],row[0],float(row[i])))
+    for i in range(len(PayerList)):
+        AllowableList.append(PayerAllowable(PayerList[i],row[0],float(row[i+1])))
 
 
 fl1.close()
@@ -54,10 +71,10 @@ fl2 = open("Claims.csv", "r")
 csvfl2 = csv.reader(fl2, delimiter = ",")
 
 
-#Populate with claims and blank billed amount
+#Populate with claims and blanks for amounts and times
 next(csvfl2) #skip header
 for row in csvfl2:
-    ClaimList.append(Claim(row[0],row[1],row[2],row[3],0))
+    ClaimList.append(Claim(row[0],row[1],row[2],row[3],0,0,0))
 
 #Update claim billed amount from allowable list
 for j in range(len(ClaimList)):
@@ -65,6 +82,14 @@ for j in range(len(ClaimList)):
         if (AllowableList[i].name == ClaimList[j].clmPayer and
                     AllowableList[i].procedure == ClaimList[j].clmProcedure):
             ClaimList[j].clmAmount = np.random.normal(AllowableList[i].allowable, 0.1*AllowableList[i].allowable)
+            #Now insert remit amounts and times
+            for k in range(len(PayerList)):
+                if FamilyList[k].name == ClaimList[j].clmPayer:
+                    ClaimList[j].remitAmount = ClaimList[j].clmAmount*(1- np.random.normal(FamilyList[k].haircut, FamilyList[k].haircut*FamilyList[k].stdHaircut))
+                    ClaimList[j].remitTime = np.random.normal(FamilyList[k].avgTime, FamilyList[k].stdTime*FamilyList[k].avgTime)
+                    FamilyList[k].count +=1
+                else:
+                    pass
         else:
             pass
 print("Claim list populated", sep='')
@@ -79,10 +104,10 @@ fl2.close()
 
 outputFile = open("OutputCSV.csv", "w")
 csvWR = csv.writer(outputFile)
-CSVheader = ["Claim Date", "Facility", "Payer", "Procedure", "Billed Amount"]
+CSVheader = ["Claim Date", "Facility", "Payer", "Procedure", "Billed Amount", "Remit Amount", "Remit Days"]
 csvWR.writerow(CSVheader)
 
 for i in range(len(ClaimList)):
-    csvWR.writerow([ClaimList[i].clmDate, ClaimList[i].clmHosp, ClaimList[i].clmPayer, ClaimList[i].clmProcedure, ClaimList[i].clmAmount])
+    csvWR.writerow([ClaimList[i].clmDate, ClaimList[i].clmHosp, ClaimList[i].clmPayer, ClaimList[i].clmProcedure, ClaimList[i].clmAmount, ClaimList[i].remitAmount, ClaimList[i].remitTime])
     print("Writing row : ", i, sep='')
 outputFile.close()
